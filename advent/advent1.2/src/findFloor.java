14ba5787fd7b13e4b0cdefe2374335da196c02be
Simon Dobner <simon.dobner@gmail.com>
2015-12-14 14:48:26 +1100

    sorted out gitignore, added advent1.2 inital commit

---
+++ advent/advent1.2/src/findFloor.java
@@ -0,0 +1,13 @@
+
+public final class findFloor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		deliveryPlan todaysPlan = new deliveryPlan();
+		
+
+	}
+
+}
