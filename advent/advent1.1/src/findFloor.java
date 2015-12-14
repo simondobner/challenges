c3621da152cf06962177e2207c06d5534239a43f
Simon Dobner <simon.dobner@gmail.com>
2015-12-14 14:36:39 +1100

    working solution to advent 1.1

--- advent/advent1.1/src/findFloor.java
+++ advent/advent1.1/src/findFloor.java
@@ -3,6 +3,10 @@ public final class findFloor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		deliveryPlan todaysPlan = new deliveryPlan();
+		
 
 	}
 
