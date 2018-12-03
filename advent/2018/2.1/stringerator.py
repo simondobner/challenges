b31cc4b23f7b91196ad39e3bdfce94cf87611acb
simon <simon.dobner@gmail.com>
2018-12-03 21:20:30 +1100

    [AOC 2018] Day 2, Part 2

--- advent/2018/2.1/stringerator.py
+++ advent/2018/2.1/stringerator.py
@@ -20,11 +20,11 @@ while not_found and len(sequences) > 1:
     counter = 1
     for x in range(0, len(sequences) - 1):
         count = sum(1 for a, b in zip(sequences[0], sequences[counter]) if a != b)
-        print(f'{sequences[0]}:{sequences[counter]}')
-        print(count)
         if count == 1:
             not_found = False
             print(set(sequences[0]).intersection(set(sequences[counter])))
+            print(f' sequence1 {sequences[0]} '
+                  f'sequence2 {sequences[counter]}')
             break
         counter += 1
     sequences.pop(0)
