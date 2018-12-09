2c60079205e45f2bfc20aeb7257e57092a60c1bd
simon <simon.dobner@gmail.com>
2018-12-09 17:02:11 +1100

    [AOC 2018] Day 4 Part 1  working solution

--- advent/2018/Day_4/sleepers.py
+++ advent/2018/Day_4/sleepers.py
@@ -1,31 +1,71 @@
+import os
+
+
 class Sleepers:
     def __init__(self):
-        self.sleep_log = {}
+        self.sleep_log = []
         self.current_guard: int = None
         self.guard_last_state_change_time: int = None
+        self.sleep_times = {}
+        self.sleepiest_guard = None
+        self.most_slept_minute: int = 0
 
     def load_sleeptime(self, file_name):
+
         with open(file_name, 'r') as infile:
             for line in infile:
-                minute = self.extract_minutes(line)
-                if '#' in line:
-                    self.change_the_guard(line)
-                    self.guard_last_state_change_time = 0
-                elif 'falls asleep' in line:
-                    print(f'guard {self.current_guard} fell_asleep at {minute}')
-                    for minutes in range(self.guard_last_state_change_time, minute):
-                        print(f'Awake at {minutes}')
-                    self.guard_last_state_change_time = minute
-                elif 'wakes up' in line:
-                    print(f'guard {self.current_guard} woke up at at {minute}')
-                    for minutes in range(self.guard_last_state_change_time, minute):
-                        print(f'Asleep at {minutes}')
-                    self.guard_last_state_change_time = minute
+                self.sleep_log.append(line)
+        self.sleep_log.sort()
 
-    def extract_minutes(self, record):
-        # [1518-11-01 00:00] Guard #10 begins shift
+    def create_sleep_time_log(self):
+
+        fell_asleep_at = 0
+        for sleeptime in self.sleep_log:
+            minute = self.extract_minutes(sleeptime)
+            if '#' in sleeptime:
+                self.change_the_guard(sleeptime)
+            elif 'falls asleep' in sleeptime:
+                fell_asleep_at = minute
+            elif 'wakes up' in sleeptime:
+                woke_up_at = minute
+                for sleep_minute in range(fell_asleep_at, woke_up_at):
+                    self.sleep_times[self.current_guard].append(sleep_minute)
+
+    @staticmethod
+    def extract_minutes(record):
         return int(record[15:17])
 
+    @staticmethod
+    def extract_day(record):
+        return int(record[10:12])
+
     def change_the_guard(self, record):
-        # [1518-11-01 00:00] Guard #10 begins shift
-        self.current_guard = record.split('#')[1].split(' ')[0]
+        guard = record.split('#')[1].split(' ')[0]
+        if guard not in self.sleep_times:
+            self.sleep_times[guard] = []
+        self.current_guard = guard
+
+    def get_most_sleepy_guard(self):
+        # TODO map, filter, something
+        most_minutes = 0
+
+        for guard in self.sleep_times:
+            if len(self.sleep_times[guard]) > most_minutes:
+                most_minutes = len(self.sleep_times[guard])
+                self.sleepiest_guard = guard
+
+    def get_most_slept_minute(self):
+        self.most_slept_minute = max(self.sleep_times[self.sleepiest_guard],
+                                     key=self.sleep_times[self.sleepiest_guard].count)
+
+
+if __name__ == '__main__':
+    guards = Sleepers()
+    guards.load_sleeptime(str(os.path.dirname(__file__)) + '/input.txt')
+    guards.create_sleep_time_log()
+    guards.get_most_sleepy_guard()
+    guards.get_most_slept_minute()
+
+    print(f'guard = {guards.sleepiest_guard }')
+    print(f'minute = { guards.most_slept_minute}')
+    print(f'result = {int(guards.sleepiest_guard) * int(guards.most_slept_minute)}')
--- advent/2018/Day_4/test/test_sleepers.py
+++ advent/2018/Day_4/test/test_sleepers.py
@@ -3,10 +3,35 @@ import os
 from sleepers import Sleepers
 
 
-def test_load_sleeptime():
+def test_load_sleeptime_sorted():
     guards = Sleepers()
     guards.load_sleeptime(str(os.path.dirname(__file__)) + '/resources/test_input.txt')
-    assert guards.current_guard == 99
+    assert guards.sleep_log == ['[1518-11-01 00:00] Guard #10 begins shift\n',
+                                '[1518-11-01 00:05] falls asleep\n',
+                                '[1518-11-01 00:25] wakes up\n',
+                                '[1518-11-01 00:30] falls asleep\n',
+                                '[1518-11-01 00:55] wakes up\n',
+                                '[1518-11-01 23:58] Guard #99 begins shift\n',
+                                '[1518-11-02 00:40] falls asleep\n',
+                                '[1518-11-02 00:50] wakes up\n',
+                                '[1518-11-03 00:05] Guard #10 begins shift\n',
+                                '[1518-11-03 00:24] falls asleep\n',
+                                '[1518-11-03 00:29] wakes up\n',
+                                '[1518-11-04 00:02] Guard #99 begins shift\n',
+                                '[1518-11-04 00:36] falls asleep\n',
+                                '[1518-11-04 00:46] wakes up\n',
+                                '[1518-11-05 00:03] Guard #99 begins shift\n',
+                                '[1518-11-05 00:45] falls asleep\n',
+                                '[1518-11-05 00:55] wakes up']
+
+
+def test_create_sleep_time_log():
+    guards = Sleepers()
+    guards.load_sleeptime(str(os.path.dirname(__file__)) + '/resources/test_input.txt')
+    guards.create_sleep_time_log()
+    assert guards.sleep_times['99'] == [40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                                        36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+                                        45, 46, 47, 48, 49, 50, 51, 52, 53, 54]
 
 
 def test_extract_mintues():
@@ -15,7 +40,34 @@ def test_extract_mintues():
     assert minutes == 36
 
 
+def test_extract_day():
+    guards = Sleepers()
+    day = guards.extract_day('[1518-11-04 00:36] falls asleep')
+    assert day == 4
+
+
 def test_extract_guard():
     guards = Sleepers()
     guards.change_the_guard('[1518-11-01 00:00] Guard #10 begins shift')
     assert guards.current_guard == '10'
+
+    assert guards.sleep_times == {'10': []}
+
+
+def test_get_most_sleepy_guard():
+    guards = Sleepers()
+    guards.load_sleeptime(str(os.path.dirname(__file__)) + '/resources/test_input.txt')
+    guards.create_sleep_time_log()
+
+    guards.get_most_sleepy_guard()
+    assert guards.sleepiest_guard == '10'
+
+
+def test_most_slept_minute():
+    guards = Sleepers()
+    guards.load_sleeptime(str(os.path.dirname(__file__)) + '/resources/test_input.txt')
+    guards.create_sleep_time_log()
+    guards.get_most_sleepy_guard()
+    guards.get_most_slept_minute()
+
+    assert guards.most_slept_minute == 24
